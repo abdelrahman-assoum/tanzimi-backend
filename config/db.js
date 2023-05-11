f7062c0b4fc75bd0419348804ad3ae87dae1a60a
abdelrahman-assoum <abdelrahman.assoum@gmail.com>
2023-05-11 16:52:38 +0300

    User Model

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        const connection = await mongoose.connect(process.env.MONGO_URL, {
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB connected Successfully`);
-    } catch (error) {
-        console.log(`MongoDB connection error: ${error}`);
-        process.exit(1);
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected Successfully`);
+  } catch (error) {
+    console.log(`MongoDB connection error: ${error}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
--- models/userModel.js
+++ models/userModel.js
@@ -1,2 +1,37 @@
-import mongoose from "mongoose";
+import mongoose, { Collection } from "mongoose";
+const {Schema, Model} = mongoose;
+
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    picture: {
+        type: String,
+        required: true,
+    }
+}, {
+       Collection: 'users',
+ });
+
+ const User = mongoose.model('User', userSchema);
+ export default User;
+
 
